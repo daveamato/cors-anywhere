0d248a390a0319f79e063b526370921113405e3d
David Amato <daveamato@live.com>
2018-05-06 16:08:31 -0400

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ['http://chowder.ml', 'http://localhost:8080', ''],
+  originWhitelist: ['http://chowder.ml', 'http://localhost', ''],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
